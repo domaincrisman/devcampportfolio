a13f180d448b1be3c672bcbeba3e91dbad0ae3f0
domaincrisman <cmanzanore@gmail.com>
2017-06-26 22:52:56 +0200

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,9 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
    	end
 	end
+
+	def copyright_generator
+		CrismanViewTool::Renderer.copyright 'Cris Manzano', 'All rights reserved'
+	end
+
 end
