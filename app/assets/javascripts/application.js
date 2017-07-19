5a2cc5b0013dcb3efd2158910000cf3cef84d9a9
domaincrisman <cmanzanore@gmail.com>
2017-07-19 10:36:35 +0200

    Integrated initial drag and drop implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require bootstrap-sprockets
 //= require jquery
 //= require jquery-ui
 //= require html.sortable
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
